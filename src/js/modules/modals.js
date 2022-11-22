5cc575157d8ee8c623db422e51cfcae28df531ce
M_Ulaonovych <m.ulanovych@gmail.com>
2022-11-22 17:31:51 +0200

    feat: write function for modal window

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -9,6 +9,7 @@ const modals = () => {
 		const modal = document.querySelector(modalSelector);
 		const close = document.querySelector(closeSelector);
 		const windows = document.querySelectorAll('[data-modal]');
+		const scroll = calcScroll();
 
 		trigger.forEach(item => {
 			item.addEventListener('click', e => {
@@ -22,6 +23,7 @@ const modals = () => {
 
 				modal.style.display = 'block';
 				document.body.classList.add('modal-open');
+				document.body.style.marginRight = `${scroll}`;
 			});
 		});
 
@@ -35,16 +37,58 @@ const modals = () => {
 		});
 
 		modal.addEventListener('click', e => {
-			if (e.target === modal) {
+			if (e.target === modal && closeClickOverlay) {
 				windows.forEach(item => {
 					item.style.display = 'none';
 				});
 
 				modal.style.display = 'none';
 				document.body.classList.remove('modal-open');
+				document.body.style.marginRight = `${0}`;
 			}
 		});
 	}
+
+	function showModalByTime(selector, time) {
+		setTimeout(() => {
+			let display;
+
+			document.querySelectorAll('[data-modal]').forEach(item => {
+				if (getComputedStyle(item).display !== 'none') {
+					display = 'block';
+				}
+			});
+
+			if (!display) {
+				document.querySelector(selector).style.display = 'block';
+				document.body.style.overflow = 'hidden';
+			}
+		}, time);
+	}
+
+	function calcScroll() {
+		let div = document.createElement('div');
+
+		div.style.width = '50px';
+		div.style.height = '50px';
+		div.style.overflow = 'scroll';
+		div.style.visibility = 'hidden';
+
+		document.body.appendChild(div);
+		let scrollWidth = div.offsetWidth - div.clientWidth;
+		div.remove();
+
+		return scrollWidth;
+	}
+
+	bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+	bindModal(
+		'.button-consultation',
+		'.popup-consultation',
+		'.popup-consultation .popup-close'
+	);
+
+	showModalByTime('.popup-consultation', 60000);
 };
 
 export default modals;
