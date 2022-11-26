2e95d2edbfc044b143a0e675e6ae05f6ecde3f25
M_Ulaonovych <m.ulanovych@gmail.com>
2022-11-26 18:51:26 +0200

    feat: add mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
+import mask from './modules/mask';
 
 window.addEventListener('DOMContentLoaded', () => {
 	modals();
@@ -12,4 +13,5 @@ window.addEventListener('DOMContentLoaded', () => {
 	);
 	sliders('.main-slider-item', 'vertical');
 	forms();
+	mask('[name="phone"]');
 });
