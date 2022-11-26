259d5ab641091e8352c798dddf2a849d138417af
M_Ulaonovych <m.ulanovych@gmail.com>
2022-11-26 16:04:31 +0200

    feat: add forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
 	modals();
@@ -10,4 +11,5 @@ window.addEventListener('DOMContentLoaded', () => {
 		'.main-next-btn'
 	);
 	sliders('.main-slider-item', 'vertical');
+	forms();
 });
