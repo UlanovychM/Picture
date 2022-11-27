b70f0c846f5321e6b13a591532fc8b5ccd349f31
M_Ulaonovych <m.ulanovych@gmail.com>
2022-11-27 14:45:04 +0200

    feat: add chekText

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
 	modals();
@@ -14,4 +15,6 @@ window.addEventListener('DOMContentLoaded', () => {
 	sliders('.main-slider-item', 'vertical');
 	forms();
 	mask('[name="phone"]');
+	checkTextInputs('[name="name"]');
+	checkTextInputs('[name="message"]');
 });
